1743dd889d9767afc37de2aafe00ec7214245a45
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-05-24 20:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your code here
 const swap_theme = () => {
+	const para = document.getElementById("ptag");
+	para.innerHTML = "I am an AccioJob student, ready to become an awesome full stack developer.";
 	const app_change = document.getElementById("app");
 	app_change.classList.toggle("night");
 	const btn_change = document.getElementById("swap");
