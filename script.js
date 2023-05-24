9e9428a3c03aea651eacc8a57ca9240a9bcaf711
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-05-24 20:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+const swap_theme = () => {
+	const app_change = document.getElementById("app");
+	app_change.classList.toggle("night");
+	const btn_change = document.getElementById("swap");
+	btn_change.classList.toggle("button_night");
+	
+}
